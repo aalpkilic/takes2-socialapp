9936f588230ff33681753dcf221b7d82f68611aa
Alp <aalpkilicc@gmail.com>
2023-11-21 21:47:14 +0300

    Add files via upload

---
+++ base/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+from . models import Room
+from django.contrib.auth.models import User
+
+
+class RoomForm(ModelForm):
+	class Meta:
+		model = Room
+		fields = '__all__'
+		exclude = ['host', 'participants']
+
+class UserForm(ModelForm):
+	class Meta:
+		model = User
+		fields = ['username', 'first_name', 'last_name', 'email']
